7bccf02c19196b5019e28bc403a3105d9af96917
Kevin Lelièvre <kevin@wizekat.eu>
2022-05-16 15:03:27 +0200

    creation page home et register
    creation pages home et register et debut des scripts associes

---
+++ Dist/scripts_front/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ 
+    document.getElementById("submit").onclick = function() {
+        var name = document.getElementById("name").value;
+        var pwd = document.getElementById("password").value;
+        //Parcours HashMap utilisateurs dans list users.
+    }
+});
