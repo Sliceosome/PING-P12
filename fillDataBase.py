b3ee30d0945d3eb975355ee01314fc63fc2c6872
gael42r <riougael42@gmail.com>
2022-06-09 17:58:37 +0200

    fill contour table 1

--- fillDataBase.py
+++ fillDataBase.py
@@ -5,12 +5,35 @@ try:
     connector = mc.connect(host = 'localhost', database = 'contour_evaluation', user = 'root', password = '')
     cursor = connector.cursor()
 
+    # # Fill "organ" table
+    # organList = os.listdir('./Dicom_Files/Contours/')
+    # # print(organList)
+
+    # for organ in organList:
+    #     request = "INSERT INTO `organ` (`name`) VALUES ('" + organ + "')"
+    #     cursor.execute(request)
+
+
+    # Fill "contour" table
     organList = os.listdir('./Dicom_Files/Contours/')
-    # print(organList)
 
     for organ in organList:
-        request = "INSERT INTO `organ` (`name`) VALUES ('" + organ + "')"
-        cursor.execute(request)
+
+        scanList = os.listdir('./Dicom_Files/Contours/' + organ + '/')
+
+        for scan in scanList:
+
+            rtStructList = os.listdir('./Dicom_Files/Contours/' + organ + '/' + scan + '/')
+
+            for rtStruct in rtStructList:
+
+                contourPath = './Dicom_Files/Contours/' + organ + '/' + scan + '/' + rtStruct
+                contourScanPath = './Dicom_Files/Scans/' + scan + '/'
+                
+                request = "INSERT INTO `contour` (`path`, `scan_path`) VALUES ('" + contourPath + "', '" + contourScanPath + "')"
+                cursor.execute(request)
+
+
 
     connector.commit()
 
