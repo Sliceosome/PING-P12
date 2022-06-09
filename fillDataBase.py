e7a49df91a82c5ac5522df45a145914efd885c6c
gael42r <riougael42@gmail.com>
2022-06-09 17:32:15 +0200

    fill organ table

--- fillDataBase.py
+++ fillDataBase.py
@@ -1,8 +1,18 @@
 import mysql.connector as mc
+import os
 
 try:
     connector = mc.connect(host = 'localhost', database = 'contour_evaluation', user = 'root', password = '')
     cursor = connector.cursor()
+
+    organList = os.listdir('./Dicom_Files/Contours/')
+    # print(organList)
+
+    for organ in organList:
+        request = "INSERT INTO `organ` (`name`) VALUES ('" + organ + "')"
+        cursor.execute(request)
+
+    connector.commit()
 
 except mc.Error as err:
     print(err)
