f90b357de10c832ff459930831e748366ee9b65f
Kevin Lelièvre <kevin@wizekat.eu>
2022-06-09 17:54:18 +0200

    affichage image depuis python
    Affiche l'image depuis un script python
    
    Pour dicom faudra voir quel tableau de pixel on récupère (RVBA, BVRA, ...)

--- public/scripts_front/process.js
+++ public/scripts_front/process.js
@@ -1,19 +1,41 @@
  $(document).ready(function(){
-    document.getElementById("toto").onclick = function() {
+    document.getElementById("search").onclick = function() {
         $.post("/request",
         {
-        name: "viSion",
-        designation: "Professional gamer"
+        number: document.getElementById("compare").checked,
+        outline: document.getElementById("outline").value
         },
         function (data, status) {
         console.log(status);
-        var zone = document.getElementById("gneu");
-        //zone.append(data.toString());
-        //var im = new ImageData(data[0][1],data[0][2])
-        //im.data = data
-        //zone.append(im);
-        console.log(data[1])
+        var zone1 = document.getElementById("cv1");
+        var zone2 = document.getElementById("cv2");
+        let im = parsingImageData(data);
+        drawCanvas(zone1,im,1)
+        drawCanvas(zone2,im,1)
         });
+    }
+    function drawCanvas(cv,im,scale){
+        console.log(im)
+        var canvas = document.getElementById(cv.id);
+        var context = canvas.getContext("2d");
+        context.putImageData(im, 0, 0,0,0,600,600);
+        if(scale !=1){
+            context.scale(scale,scale);
+            context.drawImage(canvas, 0, 0)
+        };
+    }
+
+    function parsingImageData(string){
+        let width = string[1]+string[2]+string[3]
+        width = parseInt(width);
+        let height = string[6]+string[7]+string[8]
+        height = parseInt(height);
+        string = string.substring(15);
+        let pix_array = string.split(",");
+        pix_array = new Uint8ClampedArray(pix_array);
+        console.log(pix_array)
+        var im = new ImageData(pix_array,height,width);
+        return im
     }
  });
 
--- server.js
+++ server.js
@@ -64,12 +64,13 @@ app.post('/request',(req,res)=>{
   python.stdout.on('data', function (data) {
    console.log('Pipe data from python script ...');
    dataToSend = data.toString();
-   //console.log("gneu python = " +dataToSend);
   });
   // in close event we are sure that stream from child process is closed
   python.on('close', (code) => {
   console.log(`child process close all stdio with code ${code}`);
   // send data to browser
+  const jsonData = require('./outline2.json'); 
+  dataToSend = dataToSend + jsonData
   res.send(dataToSend)
   });
 });
