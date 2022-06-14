ddeec9d58f8ace28d178798b5aa1ba6ead60835a
Kevin Lelièvre <kevin@wizekat.eu>
2022-06-14 22:31:15 +0200

    zoom et note
    implémentation du zoom, sans contrainte max, contrainte min à la taille d'origine, et reset du zoom à chaque changement de frame
    mise en place champ de notation, qu'il faudra ensuite envoyer à la bdd

--- public/scripts_front/process.js
+++ public/scripts_front/process.js
@@ -1,9 +1,12 @@
  var frames = new Array(0);
  var compt1 = 0;
  var compt2 = 0;
+ var z1 = 1;
+ var z2 = 1;
  
  $(document).ready(function(){
     document.getElementById("search").onclick = function() {
+        document.getElementById("title").innerHTML += " : " + document.getElementById("outline").value
         $.post("/request",
         {
         two: document.getElementById("compare").checked,
@@ -13,21 +16,52 @@
         console.log(status);
         var zone1 = document.getElementById("cv1");
         let im = parsingImageData(data);
-        drawCanvas(zone1,im,1)
+        drawCanvas(zone1,im,z1)
         if(document.getElementById("compare").checked){
             var zone2 = document.getElementById("cv2");
-            drawCanvas(zone2,im,1)
+            drawCanvas(zone2,im,z2)
             $("#canvas2").show();
         }
         $("#canvas1").show();
         $("#title").show();
+        $("#grade").show();
         });
     }
+
+    document.getElementById("z+1").onclick = function() {
+        z1+=0.25;
+        var zone = document.getElementById("cv1");
+        drawCanvas(zone,frames[compt1],z1);
+    }
+
+    document.getElementById("z-1").onclick = function() {
+        if(z1>1){
+            z1-=0.25;
+        }
+        var zone = document.getElementById("cv1");
+        drawCanvas(zone,frames[compt1],z1);
+    }
+
+    document.getElementById("z+2").onclick = function() {
+        z2+=0.25;
+        var zone = document.getElementById("cv2");
+        drawCanvas(zone,frames[compt2],z2);
+    }
+
+    document.getElementById("z-2").onclick = function() {
+        if(z2>1){
+            z2-=0.25;
+        }
+        var zone = document.getElementById("cv2");
+        drawCanvas(zone,frames[compt2],z2);
+    }
+
     document.getElementById("next1").onclick = function() {
         compt1++;
+        z1=1;
         var zone1 = document.getElementById("cv1");
         if(compt1 < frames.length){
-            drawCanvas(zone1,frames[compt1],1)
+            drawCanvas(zone1,frames[compt1],z1)
         }else{
             $.post("/unknown_frame",
                 {
@@ -37,18 +71,25 @@
                 console.log(status);
                 var zone1 = document.getElementById("cv1");
                 let im = parsingImageData(data);
-                drawCanvas(zone1,im,1)
+                drawCanvas(zone1,im,z1)
                 });
         }
+    }
+    
+    document.getElementById("graded").onclick = function() {
+        var grade = document.getElementById("grd").value;
+        document.getElementById("grd").value = "";
+        //TODO : send "grade" to the database in the User table, at the row of the current user of the website (the one currently connected), using POST request.
     }
 
     document.getElementById("previous1").onclick = function() {
         if(compt1 > 0){
             compt1--;
         }
+        z1=1;
         var zone1 = document.getElementById("cv1");
         if(compt1 < frames.length){
-            drawCanvas(zone1,frames[compt1],1)
+            drawCanvas(zone1,frames[compt1],z1)
         }else{
             $.post("/unknown_frame",
                 {
@@ -58,16 +99,17 @@
                 console.log(status);
                 var zone1 = document.getElementById("cv1");
                 let im = parsingImageData(data);
-                drawCanvas(zone1,im,1)
+                drawCanvas(zone1,im,z1)
                 });
         }
     }
 
     document.getElementById("next2").onclick = function() {
         compt2++;
+        z2=1;
         var zone1 = document.getElementById("cv2");
         if(compt2 < frames.length){
-            drawCanvas(zone1,frames[compt2],1)
+            drawCanvas(zone1,frames[compt2],z2)
         }else{
             $.post("/unknown_frame",
                 {
@@ -76,7 +118,7 @@
                 function (data, status) {
                 console.log(status);
                 let im = parsingImageData(data);
-                drawCanvas(zone1,im,1)
+                drawCanvas(zone1,im,z2)
                 });
         }
     }
@@ -85,9 +127,10 @@
         if(compt2 > 0){
             compt2--;
         }
+        z2=1;
         var zone1 = document.getElementById("cv2");
         if(compt2 < frames.length){
-            drawCanvas(zone1,frames[compt2],1)
+            drawCanvas(zone1,frames[compt2],z2)
         }else{
             $.post("/unknown_frame",
                 {
@@ -96,15 +139,16 @@
                 function (data, status) {
                 console.log(status);
                 let im = parsingImageData(data);
-                drawCanvas(zone1,im,1)
+                drawCanvas(zone1,im,z2)
                 });
         }
     }
 
     function drawCanvas(cv,im,scale){
-        console.log(im)
         var canvas = document.getElementById(cv.id);
         var context = canvas.getContext("2d");
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
         context.putImageData(im, 0, 0,0,0,600,600);
         if(scale !=1){
             context.scale(scale,scale);
