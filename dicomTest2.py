50e0227692456435fd0589028f82f29ad0d4698f
gael42r <riougael42@gmail.com>
2022-06-17 11:20:32 +0200

    Script images 3

--- dicomTest2.py
+++ dicomTest2.py
@@ -2,6 +2,7 @@ import pydicom as pd
 from pydicom import dcmread
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 
@@ -9,56 +10,69 @@ def load_data(sample):
     data=dict()
     # dcms data
     dcms_data=dict()
-    ds = pd.dcmread(sample['dcm_files'])  # Traverser les données lues
-    array = ds.pixel_array
-    origin = ds.ImagePositionPatient  # La position de l'origine de la grille dans le système de coordonnées mondiales.
-    spacing = ds.PixelSpacing  # Intervalle d'échantillonnage
-    uid = ds.SOPInstanceUID
-    dcms_data[uid] = {'dcmSpacing': spacing, 'dcmOrigin': origin, 'array': array}
+    for dcm_file in sample['dcm_files']:
+        # print(dcm_file)
+        ds = pd.dcmread(dcm_file)
+        array = ds.pixel_array
+        origin = ds.ImagePositionPatient
+        spacing = ds.PixelSpacing
+        uid = ds.SOPInstanceUID
+        dcms_data[uid] = {'dcmSpacing': spacing, 'dcmOrigin': origin, 'array': array}
     data['dcms_data']=dcms_data
 
     # rt data
-    rt_data = dict()  # Retourne les résultats sous forme de {RUID:label_data}.
+    rt_data = dict()
     ds = pd.dcmread(sample['rt_file'])
-    ruid = ds.ROIContourSequence[0].ContourSequence[1].ContourImageSequence[0].ReferencedSOPInstanceUID
-    array = ds.ROIContourSequence[0].ContourSequence[1].ContourData
-    num = ds.ROIContourSequence[0].ContourSequence[1].NumberOfContourPoints
-    rt_data[ruid] = {'pointNumber': num, 'array': array}
+    sequences = ds.ROIContourSequence[0].ContourSequence
+    for sequence in sequences:
+        ruid = sequence.ContourImageSequence[0].ReferencedSOPInstanceUID
+        array = sequence.ContourData
+        num = sequence.NumberOfContourPoints
+        rt_data[ruid] = {'pointNumber': num, 'array': array}
     data['rt_data']=rt_data
-    # Résultats du retour
     return data
 
 def convert_global_aix_to_net_pos(data):
-    point_data = {}  # Retourner les coordonnées {uid:data}
+    point_data = {}
     for uid, value in data['rt_data'].items():
         num = value['pointNumber']
         label_data = value['array']
         dcm_origin = data['dcms_data'][uid]['dcmOrigin']
         dcm_spacing = data['dcms_data'][uid]['dcmSpacing']
 
-        point = []  # Coordonnées [(x1,y1),(...) ,...]
-        for i in range(0,num,3):
-            x = label_data[i]  # Système de coordonnées mondiales du contour
+        point = []
+        for i in range(0, len(label_data), 3):
+            x = label_data[i]
             y = label_data[i + 1]
-            X = int((float(x) - float(dcm_origin[0])) / float(dcm_spacing[0]))  # Coordonnée X du contour
-            Y = int((float(y) - float(dcm_origin[1])) / float(dcm_spacing[1]))  # Coordonnée Y du profil
+            X = int((float(x) - float(dcm_origin[0])) / float(dcm_spacing[0]))
+            Y = int((float(y) - float(dcm_origin[1])) / float(dcm_spacing[1]))
             point.append((X, Y))
         point_data[uid] = point
     return point_data
 
-ctPath = 'D:\Télécom\FISE\Semestre 8\PING-P12\Initial_Files\CT+2RTS\$$$38200460_1.2.840.113704.7.1.0.672206377134184.1648538072.1\CT_1.2.840.113704.7.1.0.672206377134184.1648538074.245.dcm'
+ctFile = 'D:\Télécom\FISE\Semestre 8\PING-P12\Initial_Files\CT+2RTS\$$$38200460_1.2.840.113704.7.1.0.672206377134184.1648538072.1\CT_1.2.840.113704.7.1.0.672206377134184.1648538074.245.dcm'
+ctPath = 'D:\Télécom\FISE\Semestre 8\PING-P12\Initial_Files\CT+2RTS\$$$38200460_1.2.840.113704.7.1.0.672206377134184.1648538072.1'
 rtPath = 'D:\Télécom\FISE\Semestre 8\PING-P12\Initial_Files\CT+2RTS\$$$38200460_1.2.840.113704.7.1.0.672206377134184.1648538072.1\RTSTRUCT_1.2.276.0.7230010.3.1.4.346818816.1878.1648539627.475662.dcm'
 
+foldersList = os.listdir(ctPath)
+ctPathList = []
+for file in foldersList:
+    if file[:2] == "CT":
+        ctPathList.append(ctPath + "\\" + file)
+# print(ctPathList)
 
-dictDicomFiles = {"dcm_files" : ctPath, "rt_file" : rtPath}
+dictDicomFiles = {"dcm_files" : ctPathList, "rt_file" : rtPath}
+# print(dictDicomFiles)
 
 data = load_data(dictDicomFiles)
 dataPoint = convert_global_aix_to_net_pos(data)
+# print(dataPoint) 
 listDataPoint = list(dataPoint['1.2.840.113704.7.1.0.672206377134184.1648538074.245'])
+print(listDataPoint)
 
 imgRGB = np.zeros([512, 512, 3], dtype=np.uint8)
 
-ds = dcmread(ctPath)
+ds = dcmread(ctFile)
 img = ds.pixel_array
 # print("min : ", np.min(img),"max : ", np.max(img))
 imgRGB[:,:,0] = (img + 300) / 10
@@ -84,4 +98,3 @@ for point in listDataPoint:
 plt.imshow(imgRGB)
 plt.axis('off')
 plt.savefig('./test' ,bbox_inches='tight', pad_inches=0, dpi=138.7)
-
