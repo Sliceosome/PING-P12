694d6d625757ce9f05127aa9c8779ca401c891c0
Kevin Lelièvre <kevin@wizekat.eu>
2022-06-22 11:33:14 +0200

    toutes les requetes bdd passées en async
    async et await de toutes les requetes et tout fonctionne

--- CompleteSite/server/server.js
+++ CompleteSite/server/server.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const http = require('http');
-const {spawn} = require('child_process');
 const express = require('express');
 var bodyParser = require('body-parser')
 const app = express();
@@ -60,6 +58,7 @@ const config = {    //Parameters to connect to database. To change with the real
   
   var urlencodedParser = bodyParser.urlencoded({ extended: false })
   var path1 = "";
+  var id1 = ""
   var path2 = "";
   var files1 = "";
   var files2 = "";
@@ -67,9 +66,10 @@ const config = {    //Parameters to connect to database. To change with the real
   
   app.post('/request',urlencodedParser, async (req,res)=>{
     const connection2 = await initializeConnection(config);
-    let [rows,fields] = await connection2.execute('SELECT organ.name, contour.folder_path FROM organ INNER JOIN contour ON organ.id_organ = contour.id_organ WHERE organ.name = ? order by rand()',[req.body.outline])
+    let [rows,fields] = await connection2.execute('SELECT organ.name, organ.id_organ, contour.folder_path FROM organ INNER JOIN contour ON organ.id_organ = contour.id_organ WHERE organ.name = ? order by rand()',[req.body.outline])
     console.log('The solution is: ', rows[0].folder_path);
     path1 = rows[0].folder_path;
+    id1 = rows[0].id;
     path1 = path1 + "/CLASSIC"
     if(req.body.two){
         path2 = rows[1].folder_path + "/CLASSIC"
@@ -77,26 +77,6 @@ const config = {    //Parameters to connect to database. To change with the real
     connection2.end();
     console.log("path 2 : " + path2)
     console.log("path 1 : " + path1)
-    // var dataToSend;
-    // // spawn new child process to call the python script
-    // const python = spawn('python', ['test.py',"moi.PNG"]);
-    // // collect data from script
-    // python.stdout.on('data', function (data) {
-    //  console.log('Pipe data from python script ...');
-    //  dataToSend = data.toString();
-    // });
-    // // in close event we are sure that stream from child process is closed
-    // python.on('close', (code) => {
-    // console.log(`child process close all stdio with code ${code}`);
-    // // send data to browser
-    // let jsonData = require('./outline.json'); 
-    // dataToSend = dataToSend + jsonData
-    // //res.send(dataToSend)
-    // files = fs.readdirSync(path1)
-    // console.log(path.join(path1,files[0]));
-    // let imgStr =  fs.readFileSync(path.join(path1,files[0]),"base64");
-    // res.send(imgStr)
-    // });
     files1 = fs.readdirSync(path1)
     let imgStr1 =  fs.readFileSync(path.join(path1,files1[0]),"base64");
 
@@ -112,19 +92,6 @@ const config = {    //Parameters to connect to database. To change with the real
     var nb = req.body.number;
     let imgStr2 = ""
     var isTrueSet = (req.body.two === 'true');
-    // const python = spawn('python', ['test.py',"tablesturned.JPG"]);
-    // python.stdout.on('data', function (data) {
-    //   console.log('Pipe data from python script ...');
-    //   dataToSend = data.toString();
-    //  });
-    //  // in close event we are sure that stream from child process is closed
-    //  python.on('close', (code) => {
-    //  console.log(`child process close all stdio with code ${code}`);
-    //  // send data to browser
-    //  delete require.cache[require.resolve('./outline.json')]
-    //  let jsonData = require('./outline.json');
-    //  console.log(dataToSend);
-    //  dataToSend = dataToSend + jsonData
     if(nb < Math.max(files1.length,files2.length)){
         if(isTrueSet){
             imgStr2 =  fs.readFileSync(path.join(path2,files2[nb]),"base64");
@@ -133,11 +100,14 @@ const config = {    //Parameters to connect to database. To change with the real
         res.send(imgStr + "end1" + imgStr2)
     }else{res.send("Out of bounds")}
     });
-    //});
     
-  app.post('/graded',urlencodedParser,(req,res)=>{
+  app.post('/graded',urlencodedParser,async (req,res)=>{
     let mark = req.body.value;
+    mark = parseInt(mark)
     console.log("grade = " + mark);
+    const connection3 = await initializeConnection(config);
+    //await connection3.execute('')
+
   })
 
   
@@ -149,68 +119,62 @@ const config = {    //Parameters to connect to database. To change with the real
     next();
   });
 
-app.post('/register', (req, res) => {
-    db = initializeConnection(config);
+app.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
     const hashedPassword = encrypt(password);
-
-    db.query("SELECT COUNT( * ) AS existingAccount FROM users WHERE email=?", [email], function(err, rows) {
-        if (err) throw err;
-        if (rows[0].existingAccount!==0)
-        {
-            res.send("This email adress already has an account");
+    let rows = ""
+    let db =""
+    try{
+        db = await initializeConnection(config);
+        [rows] = await db.execute("SELECT COUNT( * ) AS existingAccount FROM users WHERE email=?", [email])
+    }catch(e){console.log(e)}
+    if (rows[0].existingAccount!==0)
+    {
+        res.send("This email adress already has an account");
+    }
+    else {
+        try{
+            let [rows2] = await db.execute("SELECT COUNT( * ) AS existingUsername FROM users WHERE username=?", [username])
+            if (rows2[0].existingUsername!==0)
+            {
+                res.send("Username Taken");
+            }
+            else{
+                let [result] = await db.execute("INSERT INTO users (username, password, iv, email) VALUES (?,?,?,?)", [username, hashedPassword.password, hashedPassword.iv, email])
+                res.send("Success");
+            }
+        }catch(e){console.log(e)}
         }
-        else {
-            db.query("SELECT COUNT( * ) AS existingUsername FROM users WHERE username=?", [username], function(err, rows) {
-                if (err) throw err;
-                if (rows[0].existingUsername!==0)
-                {
-                    res.send("Username Taken");
-                }
-                else{
-                    db.query("INSERT INTO users (username, password, iv, email) VALUES (?,?,?,?)", 
-                    [username, hashedPassword.password, hashedPassword.iv, email],
-                    (err, result) => {
-                        if (err) { console.log(err); }
-                        else { res.send("Success"); } 
-                    });
-                }
-            });
-        }
-        });
-});
 
-app.post('/login', (req, res) => {
+    });
+
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    db = initializeConnection(config);
-    db.query("SELECT COUNT( * ) AS existingAccount FROM users WHERE username=?;", [username], function(err, rows, fields) {
-        if (err) throw err;
-        if (rows[0].existingAccount===0)
-        {
-            res.send("There is no existing username, please sign up !");
+    let db="";
+    let rows = "";
+    try{
+        db = await initializeConnection(config);
+        [rows] = await db.execute("SELECT COUNT( * ) AS existingAccount FROM users WHERE username=?;", [username])
+    }catch(e){console.log(e)}
+    if (rows[0].existingAccount===0)
+    {
+        res.send("There is no existing username, please sign up !");
+    }
+    else {
+        try{
+            let [result] = await db.execute("SELECT * FROM users WHERE username=?;", [username])
+            const encryptedPassword = { iv: result[0].iv, password: result[0].password };
+            const decryptedPassword = decrypt(encryptedPassword);
+            if (password!==decryptedPassword)
+            {
+                res.send("This is not the right password, forgot password ?");
+            } 
+            else 
+            {
+                res.send("Logged in !");
+            }
+        }catch(e){console.log(e)}
         }
-
-        else {
-            db.query("SELECT * FROM users WHERE username=?;", 
-            [username],
-            (err, result) => {
-                if (err) { console.log(err); }
-                else {
-                    const encryptedPassword = { iv: result[0].iv, password: result[0].password };
-                    const decryptedPassword = decrypt(encryptedPassword);
-
-                    if (password!==decryptedPassword)
-                    {
-                        res.send("This is not the right password, forgot password ?");
-                    } 
-                    else 
-                    {
-                        res.send("Logged in !");
-                    }
-                }
-            });
-        }
-        });
-});
+    });
 
 app.listen(PORT, () => console.log('API is running on http://localhost:8080'));
